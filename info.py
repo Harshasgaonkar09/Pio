f420e6e9bd3f641c7684777b3a4d0dbb53e616f6
Harshasgaonkar09 <141051178+Harshasgaonkar09@users.noreply.github.com>
2023-12-22 15:32:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,8 +52,8 @@ BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 RemoveBG_API = environ.get("RemoveBG_API", "")
 
 # url shortner
-SHORT_URL = environ.get("SHORT_URL")
-SHORT_API = environ.get("SHORT_API")
+SHORT_URL = environ.get("SHORT_URL", "ipopkorn.io")
+SHORT_API = environ.get("SHORT_API", "620042d86f2ad3249b8dd6d4f37a48738068a45a")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
